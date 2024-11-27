f96b6a2c0e9858cb5ebdda7c1f2522b394c18279
foxdust42 <foxdust42@gmail.com>
2024-11-27 22:09:41 +0100

    nanomq wokring w/ autostart, z2m progress

--- buildroot-2024.02.7/flash.sh
+++ buildroot-2024.02.7/flash.sh
@@ -7,7 +7,14 @@ else
 	exit 1;	
 fi
 
-echo "will flash buildroot output to /dev/sda"
+if stat ./output/images/sdcard.img; then
+	echo ;
+else
+	echo "sd card image not found";
+	exit 1;
+fi
+
+echo "will flash buildroot output to /dev/sda (y?)"
 read agree
 
 if [ "$agree" = "y" ]; then
@@ -17,5 +24,5 @@ else
 	exit 1;
 fi
 set -x
-/bin/sudo -k dd if=./output/images/sdcard.img of=/dev/sda bs=4M progress
+/bin/sudo -k dd if=./output/images/sdcard.img of=/dev/sda bs=32M status=progress
 set +x
--- plantsense_tree/package/zigbee2mqtt/zigbee2mqtt.mk
+++ plantsense_tree/package/zigbee2mqtt/zigbee2mqtt.mk
@@ -9,11 +9,19 @@ ZIGBEE2MQTT_SITE=https://github.com/Koenkk/zigbee2mqtt.git
 ZIGBEE2MQTT_SITE_METHOD = git
 
 define ZIGBEE2MQTT_BUILD_CMDS
-	# pass
+	npm ci --prefix $(@D)
+	npm run build --prefix $(@D)
 endef
 
 define ZIGBEE2MQTT_TARGET_INSTALL_CMDS
 	mkdir /opt/zigbee2mqtt
+	cp $(@D)/* $(TARGET_DIR)/opt/zigbee2mqtt
+	cp -r $(@D)/data $(TARGET_DIR)/opt/zigbee2mqtt/data
+	cp -r $(@D)/dist $(TARGET_DIR)/opt/zigbee2mqtt/dist
+	cp -r $(@D)/lib $(TARGET_DIR)/opt/zigbee2mqtt/lib
+	cp -r $(@D)/node_modules $(TARGET_DIR)/opt/zigbee2mqtt/node_modules
+	cp -r $(@D)/scripts $(TARGET_DIR)/opt/zigbee2mqtt/scripts
+	cp -r $(@D)/images $(TARGET_DIR)/opt/zigbee2mqtt/images
 	## 
 endef
 
